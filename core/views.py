61d872df34a81e119febc3f576739657ac1ac7c5
Godson Jerry <chiemenagodson532@gmail.com>
2023-04-26 09:02:18 +0100

    First Commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+# Imports
+from rest_framework.views import APIView
+from Sunify_Predictions import predictions
+from  rest_framework.response import Response
+
+
+
+class predictView(APIView):
+    def get(self, request):
+        predict = predictions.prediction()
+        response_dict = {"Predicted_readings":predict}
+        return Response(response_dict, status=200)
+
